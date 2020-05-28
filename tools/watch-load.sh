34c9aaace1a8e2da8057923bd59bcff2b97d0858
Lucas Czech <luc@s-cze.ch>
2020-05-27 22:35:58 -0700

    Refine load watch

--- tools/watch-load.sh
+++ tools/watch-load.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-#cnt=0
+cnt=0
 while true; do 
 
-    # Print processor load and 10 most cpu consuming processes
-    uptime >> watch_uptime.log
-    (echo "%CPU %MEM ARGS $(date)" && ps -e -o pcpu,pmem,args --sort=pcpu | cut -d" " -f1-5 | tail | tac -) >> watch_ps.log
+    # Print processor load
+    uptime >> watch-uptime.log
     
     # Every 5sec, print 10 most cpu consuming processes
-    #if ! ((cnt % 5)); then
-        #(echo "%CPU %MEM ARGS $(date)" && ps -e -o pcpu,pmem,args --sort=pcpu | cut -d" " -f1-5 | tail | tac -) >> ps.log
-    #fi  
+    if ! ((cnt % 5)); then
+        (echo "%CPU %MEM ARGS $(date)" && ps -e -o pcpu,pmem,args --sort=pcpu | cut -d" " -f1-5 | tail | tac -) >> watch-ps.log
+    fi  
     
-    sleep 5
-    #sleep 1
-    #cnt=$((cnt+1))
+    sleep 1
+    cnt=$((cnt+1))
 done
 
-#while true; do uptime >> uptime.log; sleep 1; done
-#while true; do (echo "%CPU %MEM ARGS $(date)" && ps -e -o pcpu,pmem,args --sort=pcpu | cut -d" " -f1-5 | tail | tac -) >> ps.log; sleep 5; done
+#while true; do uptime >> watch-uptime.log; sleep 1; done
+#while true; do (echo "%CPU %MEM ARGS $(date)" && ps -e -o pcpu,pmem,args --sort=pcpu | cut -d" " -f1-5 | tail | tac -) >> watch-ps.log; sleep 5; done
 
 
