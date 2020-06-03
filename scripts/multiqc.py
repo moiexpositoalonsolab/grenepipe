efd532459e1000bf5c803c33ce718195215a9f21
Lucas Czech <luc@s-cze.ch>
2020-06-03 15:46:22 -0700

    Fix missing locale issue on memex, pt2

--- scripts/multiqc.py
+++ scripts/multiqc.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2.7
+
 # =================================================================================================
 #     Dependencies
 # =================================================================================================
@@ -15,12 +17,12 @@ output_name = path.basename(snakemake.output[0])
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 shell(
-    "if (locale -a | grep \"C.UTF-8\") >/dev/null 2>&1 ; then      \n"
-    "    export LC_ALL=C.UTF-8                                     \n"
-    "    export LANG=C.UTF-8                                       \n"
-    "elif (locale -a | grep \"en_US.utf8\") >/dev/null 2>&1 ; then \n"
-    "    export LC_ALL=en_US.utf8                                  \n"
-    "    export LANG=en_US.utf8                                    \n"
-    "fi                                                            \n"
+    "#if (locale -a | grep \"C.UTF-8\") >/dev/null 2>&1 ; then      \n"
+    "#    export LC_ALL=C.UTF-8                                     \n"
+    "#    export LANG=C.UTF-8                                       \n"
+    "#elif (locale -a | grep \"en_US.utf8\") >/dev/null 2>&1 ; then \n"
+    "#    export LC_ALL=en_US.utf8                                  \n"
+    "#    export LANG=en_US.utf8                                    \n"
+    "#fi                                                            \n"
     "multiqc {snakemake.params} --force -o {output_dir} -n {output_name} {input_dirs} {log}"
 )
