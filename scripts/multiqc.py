cbccafa2fbc4182c7974ccabd15f5ace52262a92
Lucas Czech <luc@s-cze.ch>
2020-06-03 15:40:30 -0700

    Fix missing locale issue on memex

--- scripts/multiqc.py
+++ scripts/multiqc.py
@@ -15,7 +15,12 @@ output_name = path.basename(snakemake.output[0])
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 shell(
-    "export LC_ALL=C.UTF-8; "
-    "export LANG=C.UTF-8; "
+    "if (locale -a | grep \"C.UTF-8\") >/dev/null 2>&1 ; then      \n"
+    "    export LC_ALL=C.UTF-8                                     \n"
+    "    export LANG=C.UTF-8                                       \n"
+    "elif (locale -a | grep \"en_US.utf8\") >/dev/null 2>&1 ; then \n"
+    "    export LC_ALL=en_US.utf8                                  \n"
+    "    export LANG=en_US.utf8                                    \n"
+    "fi                                                            \n"
     "multiqc {snakemake.params} --force -o {output_dir} -n {output_name} {input_dirs} {log}"
 )
