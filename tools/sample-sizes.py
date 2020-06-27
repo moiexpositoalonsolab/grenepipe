7d3b5c004ab6c0ab400cc18886b2ec6bd973b958
Lucas Czech <luc@s-cze.ch>
2020-06-27 01:22:52 -0700

    Refine sample size tool

--- tools/sample-sizes.py
+++ tools/sample-sizes.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python2
 
 import pandas as pd
-from tabulate import tabulate
+# from tabulate import tabulate
 import os, sys
+import gzip
 
 # Usage: Call with no arguments to load `samples.tsv` from the current dir, or provide
 # a single argument to a `samples.tsv` file or directory containing such a file.
@@ -19,12 +20,45 @@ print "Summarizing", smpfile, "\n"
 os.chdir(os.path.dirname(smpfile))
 
 # Read samples and units table
-samples = pd.read_csv(smpfile, sep='\t', dtype=str).set_index(["sample", "unit"], drop=False)
-samples.index = samples.index.set_levels([i.astype(str) for i in samples.index.levels])  # enforce str in index
+samples = pd.read_csv(smpfile, sep='\t', dtype=str)
+# samples = pd.read_csv(smpfile, sep='\t', dtype=str).set_index(["sample", "unit"], drop=False)
+# samples.index = samples.index.set_levels([i.astype(str) for i in samples.index.levels])  # enforce str in index
 
 # Add the file sizes as
-samples["fq1_size"] = [os.path.getsize(f) if os.path.isfile(str(f)) else os.path.getsize("../" + str(f)) for f in samples["fq1"]]
-samples["fq2_size"] = [os.path.getsize(f) if os.path.isfile(str(f)) else (os.path.getsize("../" + str(f)) if os.path.isfile("../" + str(f)) else 0) for f in samples["fq2"]]
+samples["fq1_size"] = [
+    os.path.getsize(str(f))
+    if os.path.isfile(str(f))
+    else os.path.getsize("../" + str(f))
+    for f in samples["fq1"]
+]
+samples["fq2_size"] = [
+    os.path.getsize(str(f))
+    if os.path.isfile(str(f))
+    else (
+        os.path.getsize("../" + str(f)) if os.path.isfile("../" + str(f)) else 0
+    ) for f in samples["fq2"]
+]
 
-# print samples
-print( tabulate( samples ))
+def count_fastq_gzip_lines(fastqfile):
+    count = 0
+    with gzip.open(fastqfile,'rt') as f:
+        for line in f:
+            count += 1
+    return int(count / 4)
+
+samples["fq1_seqs"] = [
+    count_fastq_gzip_lines(str(f))
+    if os.path.isfile(str(f))
+    else count_fastq_gzip_lines("../" + str(f))
+    for f in samples["fq1"]
+]
+samples["fq2_seqs"] = [
+    count_fastq_gzip_lines(str(f))
+    if os.path.isfile(str(f))
+    else (
+        count_fastq_gzip_lines("../" + str(f)) if os.path.isfile("../" + str(f)) else 0
+    ) for f in samples["fq2"]
+]
+
+print samples
+# print( tabulate( samples ))
